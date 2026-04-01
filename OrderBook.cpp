e12d1f7a08e7cb1f1599e93e528324dff5ef98a9
lconerado2@gmail.com <lconerado2@gmail.com>
2026-04-01 17:49:20 -0300

    Atualizando

--- OrderBook.cpp
+++ OrderBook.cpp
@@ -1,4 +1,29 @@
 #include <iostream>
 using namespace std;
+
+#include "Order.hpp" 
+#include "Transaction.hpp"
 #include "OrderBook.hpp" 
+
+
+OrderBook::OrderBook(){}
+
+OrderBook::~OrderBook(){}; 
+
+Order* OrderBook::getBuyOrders(int* n){}
+
+Order* OrderBook::getBuyOrders(int* n){}
+
+Order* OrderBook::getSellOrders(int* n){}
+
+Transaction* OrderBook::getTransactions(int* n){}
+
+void OrderBook::printBuyOrders(){
+}
+
+void OrderBook::printSellOrders(){
+}
+
+void OrderBook::printTransactions(){
+}
 
