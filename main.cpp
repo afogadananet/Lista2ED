ae94b50185f0bdd47acbf4aa307e6a416e5792d4
lconerado2@gmail.com <lconerado2@gmail.com>
2026-04-11 16:42:37 -0300

    Readme

--- main.cpp
+++ main.cpp
@@ -12,30 +12,46 @@ int main(){
     skoob.printTransactions();
 
     float valor = 10; 
-    
+
     // testes do valor 
-    Order venda1(1,'S',valor, 11);
-    Order pedido1(2, 'B', valor, 10); 
-    Order pedido(4,'B', valor, 12);
+    Order pedido(2,'S',valor,1);
+    Order pedido2(1,'B',valor-1,0);
+    Order pedido3(5,'B',valor+1,12);
+    Order pedidu(15,'B',valor,1);
+    Order venda(3,'S', valor-2, 3);
+    Order venda1(6,'S', valor-3, 13);
+    Order vendas1(16,'S', valor+10, 5);
+    Order vendas2(16,'S', valor+10, 6);
+    Order compra(11,'B', valor + 11, 1); 
 
     skoob.submit(pedido);
-    skoob.submit(pedido1);
-    skoob.submit(venda1);
+    skoob.submit(pedido2);
+    skoob.submit(venda);
 
+    skoob.printSellOrders();
     skoob.printBuyOrders(); 
-    skoob.printSellOrders(); 
-    skoob.printTransactions(); 
+    skoob.printTransactions();
 
-    int a; 
-    skoob.getBuyOrders(&a); 
-    cout << a << endl;
+    skoob.submit(pedido3); 
+    skoob.submit(venda1);
+    skoob.cancel(6); 
+    
+    skoob.submit(pedidu); 
+    skoob.submit(vendas1); 
+    skoob.submit(vendas2);
+
+
 
     int b; 
-    skoob.getSellOrders(&b); 
+    skoob.getBuyOrders(&b); 
     cout << b << endl;
 
+    int a; 
+    skoob.getTransactions(&a); 
+    cout << a << endl;
+
     int c; 
-    skoob.getTransactions(&c); 
+    skoob.getSellOrders(&c); 
     cout << c << endl;
 
     return 0;
